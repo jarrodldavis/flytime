bf77c929e51bff14af570508c3a05265fbd41275
Jarrod Davis <developer@jarrodldavis.com>
2019-04-28 13:33:17 -0600

    Use @slack/bolt
    - Install `@slack/bolt`
    - Update `server.js` to use bolt's Express receiver instead of polka

--- src/server.js
+++ src/server.js
@@ -1,20 +1,24 @@
 import sirv from 'sirv';
-import polka from 'polka';
 import compression from 'compression';
+import { App, ExpressReceiver } from '@slack/bolt';
 import * as sapper from '@sapper/server';
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, SLACK_SIGNING_SECRET } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) {
-			// eslint-disable-next-line no-console
-			console.log('error', err);
-		}
-	});
+const receiver = new ExpressReceiver({ signingSecret: SLACK_SIGNING_SECRET });
+receiver.app.use(
+	compression({ threshold: 0 }),
+	sirv('static', { dev }),
+	sapper.middleware()
+);
+
+async function authorize() {
+	// TODO
+	return Promise.reject(new Error('Authorization is not implemented'));
+}
+
+const app = new App({ receiver, authorize });
+
+// eslint-disable-next-line no-console
+app.start(PORT).catch(error => console.log('error', error));
