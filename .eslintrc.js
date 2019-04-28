e9b52525c2e4d0bef567f3fd3db5f2c26ce99fca
Jarrod Davis <developer@jarrodldavis.com>
2019-04-27 18:13:59 -0600

    Install and configure Prettier
    - Install Prettier core and linter configs
    - Configure ESLint and stylelint to not conflict with Prettier
    - Configure Prettier to use tabs, single quotes, and unix line endings
    - Configure Prettier to disregard HTML whitespace sensitivity
    - Install and patch a plugin for Svelte templates
    - Ignore non-code files, `node_modules`, and Sapper-generated
      directories

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,31 +3,13 @@ module.exports = {
 	'env': {
 		'es6': true,
 	},
-	'extends': ['eslint:recommended', 'problems/node10'],
+	'extends': ['eslint:recommended', 'problems/node10', 'prettier'],
 	'globals': {
 		'process': 'readonly',
 	},
 	'parserOptions': {
 		'ecmaVersion': 2018,
 		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
 	},
 	'overrides': [
 		{
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'ignore'
+};
