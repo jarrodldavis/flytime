d6dc12937640fca052ce04ea40fcef047d0e798b
Jarrod Davis <developer@jarrodldavis.com>
2019-05-13 22:43:33 -0600

    Add graceful shutdown

--- src/redis.js
+++ src/redis.js
@@ -3,5 +3,3 @@ import redis from 'redis';
 import { REDIS_URL } from './environment';
 
 export const redis_client = redis.createClient(REDIS_URL);
-
-redis_client.unref();
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import { promisify } from 'util';
 import express from 'express';
 import sirv from 'sirv';
 import compression from 'compression';
@@ -5,15 +7,18 @@ import * as sapper from '@sapper/server';
 
 import { slack_middleware, slack_client } from './slack';
 import { session_middleware, get_client_session_data } from './session';
+import { redis_client } from './redis';
 import { is_development } from './common';
 import { PORT } from './environment';
+
+console.log('Starting up...');
 
 function provide_slack_client(req, _res, next) {
 	req.slack_client = slack_client;
 	next();
 }
 
-express()
+const server = express()
 	.set('trust proxy', true)
 	.use(
 		slack_middleware,
@@ -23,4 +28,47 @@ express()
 		session_middleware,
 		sapper.middleware({ session: get_client_session_data })
 	)
-	.listen(PORT);
+	.listen(PORT, error => {
+		if (error) {
+			console.error('Error starting HTTP server:', error);
+		} else {
+			console.log(`HTTP server started and listening on port ${PORT}`);
+		}
+	});
+
+const close_server = promisify(server.close).bind(server);
+const quit_redis = promisify(redis_client.quit).bind(redis_client);
+
+let shutting_down = false;
+
+async function graceful_shutdown(signal) {
+	console.log(`Received ${signal}`);
+
+	if (shutting_down) {
+		console.log('Already shutting down, ignoring signal');
+		return;
+	}
+
+	shutting_down = true;
+
+	console.log('Closing HTTP server...');
+	try {
+		await close_server();
+		console.log('Successfully closed HTTP server');
+	} catch (error) {
+		console.error('Failed to close HTTP server:', error);
+	}
+
+	console.log('Quitting Redis connection...');
+	try {
+		await quit_redis();
+		console.log('Successfully quit Redis connection');
+	} catch (error) {
+		console.error('Failed to quit Redis connection:', error);
+	}
+
+	console.log('Completed shutdown successfully');
+}
+
+process.on('SIGTERM', graceful_shutdown);
+process.on('SIGINT', graceful_shutdown);
