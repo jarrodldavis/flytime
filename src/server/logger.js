c896027cf695df742f1a1b670d707a31ebc35a48
Jarrod Davis <developer@jarrodldavis.com>
2019-05-21 04:21:10 -0600

    Handle invalid log levels
    Catch and log failures to set the `pino` log level.

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,12 +1,7 @@
 import pino from 'pino';
 import { is_development, name, version } from '../common';
 
-// use `process.env` directly so that log level is set immediately
-// `pino` validates the level name so additional validation isn't needed
-const level = process.env.LOG_LEVEL; // eslint-disable-line no-process-env
-
 export const logger = pino({
-	level,
 	prettyPrint: is_development && { translateTime: true },
 	serializers: { err: pino.stdSerializers.err, error: pino.stdSerializers.err },
 	redact: ['req.headers.cookie', 'res.headers["set-cookie"]'],
@@ -14,6 +9,21 @@ export const logger = pino({
 		package: { name, version }
 	}
 });
+
+try {
+	// use `process.env` directly so that log level is set immediately
+	// `pino` validates the level name so additional validation isn't needed
+	logger.level = process.env.LOG_LEVEL; // eslint-disable-line no-process-env
+} catch (error) {
+	const exit_code = 2;
+	pino
+		.final(logger)
+		.fatal(
+			{ error, exit_code },
+			'Immediately terminating due to failure to set log level'
+		);
+	process.exit(exit_code);
+}
 
 logger.info('Logger created and ready for use');
 
