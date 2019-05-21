65f15e508a90575c6a59467bc833b20e6756a4ab
Jarrod Davis <developer@jarrodldavis.com>
2019-05-20 22:41:31 -0600

    Consolidate ESLint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
 	root: true,
+	plugins: ['svelte3'], // replace with @jarrodldavis/svelte once released
 	env: {
-		es6: true
+		'shared-node-browser': true
 	},
 	extends: ['eslint:recommended', 'problems/node10', 'prettier'],
 	parserOptions: {
 		ecmaVersion: 2019,
 		sourceType: 'module'
+	},
+	settings: {
+		'svelte3/lint-template': true
 	},
 	rules: {
 		'no-process-env': 'error',
@@ -21,9 +25,26 @@ module.exports = {
 	},
 	overrides: [
 		{
-			files: ['rollup.config.js', 'mock-redis.js'],
-			env: {
-				node: true
+			files: [
+				'rollup.config.js',
+				'mock-redis.js',
+				'src/server.js',
+				'src/server/*.js'
+			],
+			env: { node: true }
+		},
+		{
+			files: ['src/client.js'],
+			env: { browser: true }
+		},
+		{
+			files: ['src/service-worker.js'],
+			env: { serviceworker: true }
+		},
+		{
+			files: ['**/*.svelte'],
+			rules: {
+				'no-labels': 'off'
 			}
 		}
 	]
